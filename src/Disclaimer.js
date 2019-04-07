b7a84b5f74645038b7d27c7519755ef7f9534908
Zhenya Mamenko <mamenko@gmail.com>
2019-04-07 00:45:54 -0700

    Disclaimer

---
+++ src/Disclaimer.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Disclaimer.css';
+
+const Disclaimer = ({ children }) => (
+	<div className="disclaimer">
+		<div className="container">
+			<div className="row">
+				<div className="col-md-12 col-sm-12 col-xs-12">
+				{children}
+				</div>
+			</div>
+		</div>
+	</div>
+);
+
+
+export default Disclaimer;
