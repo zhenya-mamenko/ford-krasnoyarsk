dee703ecd1caeeaade6635a4ad3fb1d1c74129f7
Zhenya Mamenko <mamenko@gmail.com>
2019-04-08 16:25:28 -0700

    List

--- src/List.js
+++ src/List.js
@@ -6,34 +6,22 @@ import Card from './Card';
 const List = ({ items }) => (
 	<div className="col-12">
 		<div className="row">
-			<Card
-				key="card1"
-				image="/i/cars/kuga/kuga-1.jpg"
-				name="Ford Kuga Ambiente"
-				description={
-					<React.Fragment>
-						Цвет:&nbsp;&nbsp;черный металлик<br />
-						Двигатель:&nbsp;&nbsp;2.5 л<br />
-						Коробка:&nbsp;&nbsp;автоматическая
-					</React.Fragment>
-				}
-				buttons={[
-					{ type: "benefit", caption: <span>Выгода до 250 000 руб.<sup>2</sup></span>, link: "#order-link" },
-					{ type: "price", caption: "ПОЛУЧИТЬ СПЕЦПРЕДЛОЖЕНИЕ", link: "#order-link" },
-				]}
-			>
-				<React.Fragment>
-					<p>Дополнительное<br />оборудование</p>
-					<b>Пакет &laquo;Комфорт&raquo;: </b>
-					<ul>
-						<li>Электроподогрев передних сидений</li>
-						<li>Электрообогрев лобового стекла и зоны покоя стеклоочистителей</li>
-						<li>Электрообогрев рулевого колеса</li>
-						<li>2-зонный климат-контроль</li>
-						<li>Электронный стояночный тормоз</li>
-					</ul>
-				</React.Fragment>
-			</Card>
+			{ items.map((e, i) =>
+				<Card
+					key={ "card" + i }
+					image={ e.image }
+					name={ e.name }
+					description={ e.description }
+					buttons={ e.buttons }
+				>
+					{ e.gift !== false ?
+						[<React.Fragment>,
+						e.gift,
+						</React.Fragment>]
+						: false
+					}
+				</Card>
+			) }
 		</div>
 	</div>
 );
